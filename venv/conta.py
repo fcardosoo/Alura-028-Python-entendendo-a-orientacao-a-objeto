5a9a8fbc6d1c992b6555baf8aad6c5721712b46d
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-01-12 11:43:30 -0300

    Update conta.py

--- venv/conta.py
+++ venv/conta.py
@@ -7,4 +7,8 @@ class Conta:
         self.limite = limite
 
     def extrato(self):
-        print('Saldo {} do titular {}'.format(self.saldo, self.titular))
+        print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+        print('Novo saldo de {} do titular {}'.format(self.saldo, self.titular))
